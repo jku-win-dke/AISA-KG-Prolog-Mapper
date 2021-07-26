7673a8daf305bb8ad5ee14ed0a66ba4a77c653c1
Marlene Hartmann <marlene@hth.at>
2021-07-26 15:53:12 +0200

    Add mapping method B

---
+++ at.jku.dke.aisa.mapperB/src/at/jku/dke/aisa/mapperB/Mapper.java
@@ -0,0 +1,453 @@
+package at.jku.dke.aisa.mapperB;
+
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.jena.graph.Graph;
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.Triple;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.shacl.Shapes;
+import org.apache.jena.shacl.parser.Shape;
+import org.apache.jena.shared.PrefixMapping;
+import org.apache.jena.util.iterator.ExtendedIterator;
+
+/**
+ * The main task of the Mapper is to create SPARQL queries and Prolog facts.
+ * 
+ * Takes shacl graph, parsed shacl shapes, prefixes and the graph name as input.
+ * For each target shape as input a KnowledgeGraphClass is created and for each property of the shape a KnowledgeGraphProperty.
+ * KnowledgeGraphClass and KnowledgeGraphProperty are used for easier creation of SPARQL queries and Prolog facts.
+ */
+public class Mapper {
+
+	private Graph shaclGraph;
+	private Shapes shapes;
+	private PrefixMapping prefixes;
+	private Map<String, String> orderedPrefixes;
+	private String graphName;
+	private List<KnowledgeGraphClass> knowledgeGraphClasses = new ArrayList<>();
+	private List<Node> superClasses = new ArrayList<>();
+	private Map<String, String> subNodeOfSuperNode = new HashMap<>();
+	
+	/**
+	 * Creates the mapper and all required variables for later use.
+	 * @param shaclGraph
+	 * @param shapes
+	 * @param prefixesFile
+	 * @param graphName
+	 */
+	public Mapper(Graph shaclGraph, Shapes shapes, String prefixesFile, String graphName) {
+		this.shaclGraph = shaclGraph;
+		this.shapes = shapes;
+		this.prefixes = RDFDataMgr.loadGraph(prefixesFile).getPrefixMapping();
+		this.orderedPrefixes = orderPrefixes(prefixes.getNsPrefixMap());
+		this.graphName = graphName;
+		findSuperClasses();
+		generateClassesAndProperties();
+	}
+
+	/**
+	 * Print static content needed to execute the file in Prolog.
+	 * 
+	 * e.g.: modules used in Prolog or flags
+	 */
+	public void printStaticContent(PrintWriter printWriter) {
+		printWriter.println("/* use the new RDF-DB library ");
+		printWriter.println("https://www.swi-prolog.org/pldoc/man?section=semweb-rdf11 */");
+		printWriter.println(":- use_module(library(semweb/rdf11)).");
+		printWriter.println();
+		printWriter.println("/* for writing/reading RDF files in Turtle format we use the Turtle library");
+		printWriter.println("https://www.swi-prolog.org/pldoc/man?section=turtle */");
+		printWriter.println(":- use_module(library(semweb/turtle)). ");
+		printWriter.println();
+		printWriter.println("/* do not output bindings for anonymous variables (e.g., _X) in query results ");
+		printWriter.println("https://www.swi-prolog.org/pldoc/man?section=flags#flag:toplevel_print_anon */");
+		printWriter.println(":- set_prolog_flag(toplevel_print_anon, false).");
+		printWriter.println();
+		printWriter.println("/* PREFIX HANDLING ");
+		printWriter.println("see: https://www.swi-prolog.org/pldoc/doc/_SWI_/library/semweb/rdf_prefixes.pl");
+		printWriter.println("*/");
+		printWriter.println();
+		printWriter.println("/* declare namespace prefixes - in addition to predeclared ones ");
+		printWriter.println("https://www.swi-prolog.org/pldoc/doc_for?object=rdf_register_prefix/2");
+		printWriter.println("*/");
+	}
+	
+	/**
+	 * Takes all target shapes of the shacl graph as input and generates the respective knowledge graph classes and properties.
+	 * This method is called at creation time of the mapper.
+	 */
+	private void generateClassesAndProperties() {
+		for(Shape rootShape : shapes.getTargetShapes()) {
+			boolean isSuperClass = superClasses.contains(rootShape.getShapeNode());
+			KnowledgeGraphClass knowledgeGraphClass = new KnowledgeGraphClass(shaclGraph, rootShape, isSuperClass, graphName);
+			knowledgeGraphClasses.add(knowledgeGraphClass);
+		}
+	}
+	
+	/**
+	 * Prints the sparql queries with the given PrintWriter.
+	 * @param printWriter
+	 */
+	public void generateSPARQLQueries(PrintWriter printWriter) {
+		for(KnowledgeGraphClass knowledgeGraphClass : knowledgeGraphClasses) {
+			printWriter.print(generatePrefixes());
+			printWriter.println(knowledgeGraphClass.getSPARQLQuery() + '\n');
+		}
+	}
+
+	/**
+	 * For each query the prefixes are generated.
+	 * 
+	 * e.g.: PREFIX  xsd:  <http://www.w3.org/2001/XMLSchema#>
+	 * 
+	 */
+	private String generatePrefixes() {
+		String prefixes = "";
+		for(Entry<String, String> entry : orderedPrefixes.entrySet()) {
+			prefixes += "PREFIX " + entry.getKey() + ": <" + entry.getValue() + ">" + '\n';
+		}
+		prefixes += '\n';
+		return prefixes;
+	}
+	
+	/**
+	 * Takes the querySolution as input and creates Prolog facts out of it.
+	 * For generation of the Prolog facts, the dedicated shacl schema is required to use information like minCount/maxCount, subClasses, etc. 
+	 * 
+	 * @param querySolution
+	 * @param schemaForGeneratingFacts
+	 * @return
+	 */
+	public String generateFact(QuerySolution querySolution, KnowledgeGraphClass schemaForGeneratingFacts) {
+		String fact = schemaForGeneratingFacts.getNameOfTargetsWithPrefixShortAndUnderScore() + "(";
+		fact += resolvePrefixMapping(querySolution.get("?graph")) != null ? resolvePrefixMapping(querySolution.get("?graph")) : ("\'" + querySolution.get("?graph") + "\'");
+		fact += resolvePrefixMapping(querySolution.get("?" + schemaForGeneratingFacts.predicateName)) != null ? (", " +resolvePrefixMapping(querySolution.get("?" + schemaForGeneratingFacts.predicateName))) : (", \'" + querySolution.get("?" + schemaForGeneratingFacts.predicateName) + "\'");
+		for(KnowledgeGraphProperty property : schemaForGeneratingFacts.getKnowledgeGraphProperties()) {
+			if(property.maxCount == 0) {
+				RDFNode node = querySolution.get("?" + property.getName() + "Concat");
+				if(node != null) {
+					fact += ", [";
+					String[] listItems = node.toString().split(",");
+					for(int i = 0; i<listItems.length; i++) {
+						String nodeWithPrefixMapping = node != null ? resolvePrefixMapping(listItems[i]) : null;
+						if(nodeWithPrefixMapping != null) {
+							fact += nodeWithPrefixMapping;
+						} else {
+							fact += "\'" + listItems[i] + "\'";
+						}
+						if(i != listItems.length-1) {
+							fact += ", ";
+						}
+					}
+					fact += "]";
+				} else {
+					fact += ", []";
+				}
+			} else {
+				RDFNode node = querySolution.get("?" + property.getName());
+				String nodeWithPrefixMapping = node != null ? resolvePrefixMapping(node) : null;
+				if(node != null) {
+					if(node.isLiteral()) {
+						if(node.toString().startsWith("nil:")) {
+							fact += ", nil(\'" + node.toString().substring(new String("nil:").length()) + "\')";							
+						} else if(node.toString().startsWith("val:")) {
+							fact += ", val(\'" + node.toString().substring(new String("val:").length()) + "\')";
+						} else if(node.toString().startsWith("xval:")) {
+							String[] xval = node.toString().split(":");
+							fact += ", "+ xval[0] + "(\'" + xval[1] + "\',\'" + xval[2] + "\')";
+						}
+					} else {
+						if(nodeWithPrefixMapping != null) {
+							fact += ", " + nodeWithPrefixMapping;
+						} else {
+							fact += ", \'" + node + "\'";
+						}
+					}
+				} else {
+					fact += ", \'$null$\'";
+				}
+			}
+		}
+		fact += ").";
+		return fact;
+	}
+	
+	/**
+	 * Replaces the URI of the prefix mapping from a list item with the key.
+	 * 
+	 * e.g.: s1:'AHY_EADH_PERMIT'
+	 * 
+	 * @param listItem
+	 * @return
+	 */
+	private String resolvePrefixMapping(String listItem) {
+		for(Entry<String, String> prefixMapping : orderedPrefixes.entrySet()) {
+			if(listItem.startsWith(prefixMapping.getValue())) {
+				return prefixMapping.getKey() + ":\'" + listItem.substring(prefixMapping.getValue().length()) + "\'";
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Replaces the URI of the prefix mapping from a RDFNode (graph) with the key.
+	 * 
+	 * e.g.: g1:'EA_AIP_DS_FULL_20170701.xml'
+	 * 
+	 * @param node
+	 * @return
+	 */
+	private String resolvePrefixMapping(RDFNode node) {
+		for(Entry<String, String> prefixMapping : orderedPrefixes.entrySet()) {
+			if(node.toString().startsWith(prefixMapping.getValue())) {
+				return prefixMapping.getKey() + ":\'" + node.toString().substring(prefixMapping.getValue().length()) + "\'";
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Orders the given prefixes from the prefix file by the length of the URI in descending order.
+	 * @param nsPrefixMap
+	 * @return
+	 */
+	private Map<String, String> orderPrefixes(Map<String, String> nsPrefixMap) {
+		Map<String, String> sortedMap = nsPrefixMap.entrySet().stream()
+		        .sorted((a, b) -> Integer.compare(b.getValue().length(), a.getValue().length()))
+		     .collect(Collectors.toMap(Map.Entry::getKey,
+		                Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+        return sortedMap;
+	}
+
+	/**
+	 * Find all shacl shapes, which have sub classes.
+	 */
+	private void findSuperClasses() {
+		ExtendedIterator<Triple> superClassesTriples = shaclGraph.find(null, ShaclUtil.rdfsSubClassOfAsNode(), null);
+		while(superClassesTriples.hasNext()) {
+			Triple triple = superClassesTriples.next();
+			Node superClass = triple.getObject();
+			if(!superClasses.contains(superClass)) {
+				superClasses.add(superClass);
+			}
+			
+			String key = null;
+			String value = null;
+			for(Entry<String, String> entry : orderedPrefixes.entrySet()) {
+				if(triple.getSubject().getNameSpace().equals(entry.getValue())) {
+					key = entry.getKey() + ":" + StringUtils.capitalize(triple.getSubject().getLocalName());
+				}
+				if(triple.getObject().getNameSpace().equals(entry.getValue())) {
+					value = entry.getKey() + ":" + StringUtils.capitalize(triple.getObject().getLocalName());
+				}
+			}
+			if(key != null && value != null) {
+				subNodeOfSuperNode.put(key, value);
+			}
+		
+		}
+	}
+	
+	/**
+	 * Returns all knowledgeGraphClasses.
+	 * @return
+	 */
+	public List<KnowledgeGraphClass> getKnowledgeGraphClasses() {
+		return this.knowledgeGraphClasses;
+	}
+
+	/**
+	 * Prints rdf_register_prefix for all required prefixes defined in the prefix file.
+	 * 
+	 * e.g.: :- rdf_register_prefix(sh,'http://www.w3.org/ns/shacl#').
+	 * 
+	 * @param printWriter
+	 */
+	public void printPrefixRegistration(PrintWriter printWriter) {
+		for(Entry<String, String> entry : orderedPrefixes.entrySet()) {
+			printWriter.println(":- rdf_register_prefix(" + entry.getKey() + ",\'" + entry.getValue() + "\').");
+		}
+		printWriter.println();
+	}
+
+	/**
+	 * Creates rdf_meta rule for each KnowledgeGraphClass.
+	 * 
+	 * e.g.: :- rdf_meta
+	 *	  % city(Graph, City, Name?, Annotation*)
+	 *	  city(r,r,t,t)
+	 * 
+	 * @param printWriter
+	 */
+	public void printRDFMeta(PrintWriter printWriter) {
+		printWriter.println("/* for prefix handling: declare predicates that have RDF terms as arguments");
+		printWriter.println("see: https://www.swi-prolog.org/pldoc/man?predicate=rdf_meta/1");
+		printWriter.println("*/");
+		printWriter.print(":- rdf_meta"); 
+		for(int i = 0; i < knowledgeGraphClasses.size(); i++) {
+			printWriter.println();
+			printWriter.println("  " + knowledgeGraphClasses.get(i).generateComment());
+			String properties = "";
+			for(int j = 0; j<knowledgeGraphClasses.get(i).getKnowledgeGraphProperties().size(); j++) {
+				properties += ",t";
+			}
+			if(i == 0 ) {
+				printWriter.println("  " + knowledgeGraphClasses.get(i).getNameOfTargetsWithPrefixShortAndUnderScore() + "(r,r" + properties + ")");				
+			} else {
+				printWriter.println("  ," + knowledgeGraphClasses.get(i).getNameOfTargetsWithPrefixShortAndUnderScore() + "(r,r" + properties + ")");
+			}
+		}
+		printWriter.println(".");
+		printWriter.println();
+	}
+	
+	/**
+	 * Generates and prints the inheritance rules of every subClass, which is a KnowledgeGraphClass.
+	 * 
+	 * e.g.: 
+	 * aixm_AirportHeliportResponsibilityOrganisation_Combined(Graph, AirportHeliportResponsibilityOrganisation, Annotation, SpecialDateAuthority, TimeInterval, Role, TheOrganisationAuthority) :-
+  	 * 		aixm_AirportHeliportResponsibilityOrganisation(Graph, AirportHeliportResponsibilityOrganisation, Role, TheOrganisationAuthority),
+  	 * 		aixm_PropertiesWithSchedule(Graph, AirportHeliportResponsibilityOrganisation, Annotation, SpecialDateAuthority, TimeInterval) .
+
+	 * 
+	 * @param printWriter
+	 */
+	public void printInheritanceRules(PrintWriter printWriter) {
+		for(KnowledgeGraphClass knowledgeGraphClass : knowledgeGraphClasses) {
+			String subClass = null;
+			KnowledgeGraphClass superClass = null;
+			for(String key : subNodeOfSuperNode.keySet())  {
+				if(key.equals(knowledgeGraphClass.getNameOfTargetsWithPrefixShort())) {
+					subClass = key;
+					superClass = getKnowledgeGraphClassByShortPrefixAndName(subNodeOfSuperNode.get(key));
+				}
+			}
+			if(subClass != null) {
+				String properties = "Graph, " + StringUtils.capitalize(knowledgeGraphClass.predicateName);
+				for(KnowledgeGraphProperty property : superClass.getKnowledgeGraphProperties()) {
+					properties += ", " + StringUtils.capitalize(property.getName());
+				}
+				for(KnowledgeGraphProperty property : knowledgeGraphClass.getKnowledgeGraphProperties()) {
+					properties += ", " + StringUtils.capitalize(property.getName());
+				}
+				printWriter.println(knowledgeGraphClass.getNameOfTargetsWithPrefixShortAndUnderScore() + "_Combined(" + properties + ") :-");
+				
+				printWriter.println("  " + knowledgeGraphClass.generatePrologRule(null) + ",");
+				//printWriter.println("  " + superClass.generatePrologRule(StringUtils.capitalize(knowledgeGraphClass.predicateName)) + " .");
+				printWriter.println("  " + generateSuperClassPart(superClass, knowledgeGraphClass) + " .");
+				printWriter.println();
+			}
+		}
+	}
+	
+	/**
+	 * Checks if the previously called superClass from printInheritanceRules does also have a superClass and generates the according output.
+	 * This method is called by printInheritanceRules only.
+	 * 
+	 * e.g.: if originalSuperClass does have a superClass:
+	 * aixm_Surface_Combined(Graph, Surface, Patch, HorizontalAccuracy, Annotation) :-
+  	 *		aixm_Surface(Graph, Surface, HorizontalAccuracy, Annotation),
+  	 *		gml_Surface_Combined(Graph, SurfacePatch, Patch) . <------------------- returns this line
+  	 *
+  	 * e.g.: if originalSuperClass does not have a superClass:
+  	 * gml_Surface_Combined(Graph, Surface, Patch) :-
+  	 *		gml_Surface(Graph, Surface, Patch),
+  	 *		gml_SurfacePatch(Graph, Surface) .  <------------------- returns this line
+  	 *
+	 * 
+	 * @param originalSuperClass
+	 * @param knowledgeGraphClass
+	 * @return
+	 */
+	private String generateSuperClassPart(KnowledgeGraphClass originalSuperClass, KnowledgeGraphClass knowledgeGraphClass) {
+		String subClass = null;
+		KnowledgeGraphClass superClass = null;
+		for(String key : subNodeOfSuperNode.keySet())  {
+			if(key.equals(originalSuperClass.getNameOfTargetsWithPrefixShort())) {
+				subClass = key;
+				superClass = getKnowledgeGraphClassByShortPrefixAndName(subNodeOfSuperNode.get(key));
+			}
+		}
+		if(subClass != null) {
+			String properties = "Graph, " + StringUtils.capitalize(superClass.predicateName);
+			for(KnowledgeGraphProperty property : superClass.getKnowledgeGraphProperties()) {
+				properties += ", " + StringUtils.capitalize(property.getName());
+			}
+			for(KnowledgeGraphProperty property : originalSuperClass.getKnowledgeGraphProperties()) {
+				properties += ", " + StringUtils.capitalize(property.getName());
+			}
+			return originalSuperClass.getNameOfTargetsWithPrefixShortAndUnderScore() + "_Combined(" + properties + ")";
+		} else {
+			return originalSuperClass.generatePrologRule(StringUtils.capitalize(knowledgeGraphClass.predicateName));
+		}
+	}
+	
+	/**
+	 * Takes prefix mapping short + name as input and returns the respective KnowledgeGraphClass.
+	 * 
+	 * e.g. input name: aixm:City
+	 * returns: the aixm:City KnowledgegraphClass
+	 * 
+	 * If there is no KnowledgeGraphClass with such a name, the method returns null.
+	 * 
+	 * @param name
+	 * @return
+	 */
+	private KnowledgeGraphClass getKnowledgeGraphClassByShortPrefixAndName(String name) {
+		for(KnowledgeGraphClass knowledgeGraphClass : knowledgeGraphClasses) {
+			if((name.equals(knowledgeGraphClass.getNameOfTargetsWithPrefixShort()))) {
+				return knowledgeGraphClass;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * Generates static Prolog methods necessary for list handling.
+	 * 
+	 * @param printWriter
+	 */
+	public void generateStaticContent(PrintWriter printWriter) {
+		printWriter.println("convert(ConcatenatedString,ListOfAtoms) :-");
+		printWriter.println("  ConcatenatedString = literal(XConcatenatedString),");
+		printWriter.println("  split_string(XConcatenatedString, ',', '', ListOfStrings),");
+		printWriter.println("  maplist(string_atom, ListOfStrings, ListOfAtoms).");
+		printWriter.println();
+		printWriter.println("convert(Null,[]) :-");
+		printWriter.println("  Null = '$null$'.");
+		printWriter.println();
+		printWriter.println("string_atom(X,Y) :- atom_string(Y,X).");
+		printWriter.println();
+	}
+
+	/**
+	 * Generates and prints the Prolog rules for each KnowledgeGraphClass.
+	 * 
+	 * @param printWriter
+	 */
+	public void generatePrologRules(PrintWriter printWriter) {
+		for(KnowledgeGraphClass knowledgeGraphClass : knowledgeGraphClasses) {
+			printWriter.println(knowledgeGraphClass.generateComment());
+			printWriter.println();
+			printWriter.println(knowledgeGraphClass.generatePrologRule(null) + " :-");
+			printWriter.println("  sparql_query(");
+			printWriter.println("      '");
+			printWriter.println(generatePrefixes());
+			printWriter.println(knowledgeGraphClass.getSPARQLQuery());
+			printWriter.println("      '");
+			printWriter.println(knowledgeGraphClass.getEndPartOfPrologRule());
+			printWriter.println();
+		}
+	}
+}
---
+++ at.jku.dke.aisa.mapperB/src/at/jku/dke/aisa/mapperB/Shacl2PrologLauncher.java
@@ -0,0 +1,187 @@
+package at.jku.dke.aisa.mapperB;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import org.apache.jena.graph.Graph;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdfconnection.RDFConnectionFactory;
+import org.apache.jena.rdfconnection.RDFConnectionFuseki;
+import org.apache.jena.shacl.Shapes;
+import org.jpl7.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Main class to map shacl shapes and data to sparql queries and Prolog facts.
+ * An additional prefix file is used for the prefix mapping of the facts.
+ * 
+ * Input files: shacl shapes, data and prefixes
+ * Output files: sparql queries, Prolog facts
+ */
+public class Shacl2PrologLauncher {
+
+	private static final int NUMBER_OF_DATA_COPIES = 10;
+	private static final String INPUT_DATA = "input/data";
+	private static final String INPUT_SCHEMA = "input/schema";
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Shacl2PrologLauncher.class);
+
+	private final static String LOCALHOST_3030 = "http://localhost:3030/test/";
+	
+	private final static String PREFIXES_FILE = "input/prefixes.ttl";
+	
+	private final static String SCHEMA_GRAPH_NAME = "https://github.com/jku-win-dke/aisa/graphs/schema";
+//	private final static String DATA_GRAPH_NAMESPACE = "https://github.com/jku-win-dke/aisa/graphs/data";
+	private final static String DATA_GRAPH_NAMESPACE = "https://github.com/jku-win-dke/aisa/graphs";//BN
+	
+	private final static String PROLOG_FILE = "output/sparql.pl";
+	
+	public static void main(String[] args) {
+		
+		// can be configured to start jena fuseki server otherwise just start jena fuseki manually
+//		ProcessBuilder builder = new ProcessBuilder("cmd", "/c", this.floraBatchFileInWorkingDir);
+//		builder.directory( new File(this.workingDir) );
+//		builder.redirectErrorStream(true); // so we can ignore the error stream
+// 		builder.start();
+//		Process process;
+//		process = builder.start();
+//		process.destroy();
+		
+		long startTime = System.currentTimeMillis();
+		
+		// jena fuseki setup
+		RDFConnectionFuseki fuseki = RDFConnectionFactory.connectFuseki(LOCALHOST_3030);
+
+		long time_one = System.currentTimeMillis();
+		
+		File dir = new File(INPUT_SCHEMA);     
+		File[] files = dir.listFiles();
+		for (int i = 0; i < files.length; i++) {
+		  File file = files[i];
+		  fuseki.load(SCHEMA_GRAPH_NAME, file.getAbsolutePath());
+		}
+		
+		long time_two = System.currentTimeMillis();
+		
+		for(int j = 0 ; j<NUMBER_OF_DATA_COPIES ; j++) {
+			File dir2 = new File(INPUT_DATA);     
+			File[] files2 = dir2.listFiles();
+			for (int i = 0; i < files2.length; i++) {
+			  File file2 = files2[i];
+//			  fuseki.load(DATA_GRAPH_NAMESPACE + "/" + j + "/" + file2.getName(), file2.getAbsolutePath());
+			  fuseki.load(DATA_GRAPH_NAMESPACE + "/" + j + "_" + file2.getName(), file2.getAbsolutePath());//BN
+			}
+		}
+		
+		long time_three = System.currentTimeMillis();
+		
+		Model model = fuseki.fetch(SCHEMA_GRAPH_NAME);
+		Graph shapesGraph = model.getGraph();
+		Shapes shapes = Shapes.parse(shapesGraph);
+		
+		long time_four = System.currentTimeMillis();
+		
+		// create KnowledgeGraphClasses and KnowledgeGraphProperties
+		Mapper mapper = new Mapper(shapesGraph, shapes, PREFIXES_FILE, SCHEMA_GRAPH_NAME);
+		
+		long time_five = System.currentTimeMillis();
+		
+		// create SPARQL file
+		File sparqlFile = createFile(PROLOG_FILE);
+		try(PrintWriter printWriter = new PrintWriter(sparqlFile)) {
+		
+			mapper.generateStaticContent(printWriter);
+			mapper.generatePrologRules(printWriter);
+			mapper.printInheritanceRules(printWriter);
+				
+		} catch (FileNotFoundException e) {
+			String message = String.format("File %s could not be found.", PROLOG_FILE);
+			LOGGER.debug(message);
+		} finally {
+			LOGGER.debug("Shacl2Sparql mapping completed. Result can be found in " + PROLOG_FILE);
+		}
+			
+		long time_six = System.currentTimeMillis();
+
+		new Query("consult('output/program.pl')").hasSolution();
+		
+		long time_eight = System.currentTimeMillis();
+		
+		new Query("run").hasSolution();
+		
+		long time_nine = System.currentTimeMillis();
+		
+		new Query("save").hasSolution();
+
+		long time_ten = System.currentTimeMillis();
+
+
+		
+		
+		fuseki.load("http://ex.org/new", "output/output.ttl");
+
+        long endTime = System.currentTimeMillis();        
+        long timeElapsed = endTime - startTime;
+        
+//        System.out.println("Jena Fuseki connection establishment: " + (time_one - startTime));
+//        System.out.println("Loading shacl schema files: " + (time_two - time_one));
+//        System.out.println("Loading data files: " + (time_three - time_two));
+//        System.out.println("Fetching shacl schema: " + (time_four - time_three));
+//        System.out.println("Creating KnowledgeGraphClasses and KnowledgeGraphProperties: " + (time_five - time_four));
+//        System.out.println("Creating SPARQL file: " + (time_six - time_five));
+//        System.out.println("Executing SPARQL queries and creating Prolog facts: " + (time_seven - time_six));
+//        System.out.println("Consult Program: " + (time_eight - time_seven));
+//        System.out.println("Invoke run/0 in Prolog: " + (time_nine - time_eight));
+//        System.out.println("Invoke save/0 in Prolog: " + (time_ten - time_nine));
+//        System.out.println("Load saved results to Fuseki: " + (endTime - time_ten));
+        System.out.println();
+        System.out.println("Execution time in milliseconds: " + timeElapsed);        
+        
+        
+        fuseki.fetch("http://ex.org/new").write(System.out, "TURTLE");
+        
+
+        /* in ein Log-File schreiben und dann in z.B. Excel auswerten */
+//        System.out.println(
+//        		System.currentTimeMillis()
+//        		+ ";A"
+//        		+ ";" + NUMBER_OF_DATA_COPIES
+//        		+ ";" + (time_one - startTime)
+//           		+ ";" + (time_two - time_one)
+//           		+ ";" + (time_three - time_two)
+//           		+ ";" + (time_four - time_three)
+//           		+ ";" + (time_five - time_four)
+//           		+ ";" + (time_six - time_five)
+//           		+ ";" + (time_seven - time_six)
+//           		+ ";" + (time_eight - time_seven)
+//           		+ ";" + (time_nine - time_eight)
+//           		+ ";" + (time_ten - time_nine)
+//           		+ ";" + timeElapsed);
+        
+        
+	}
+
+	/**
+	 * Creates a new File and deletes old content if the File already exists.
+	 * @return
+	 */
+	private static File createFile(String name) {
+		File file = new File(name);
+		try {
+			if(!file.createNewFile()) {
+				// delete old content of generated file
+				new PrintWriter(name).close();
+			}
+		} catch (FileNotFoundException e) {
+			String message = String.format("File %s coult not be found.", name);
+			LOGGER.debug(message);
+		} catch (IOException e) {
+			String message = String.format("Error when creating %s.", name);
+			LOGGER.debug(message);
+		}
+		return file;
+	}
+}
