e207f7060cd4198d9b8ff2301ed202c8f24b9d13
Marlene Hartmann <marlene@hth.at>
2021-09-20 11:14:52 +0200

    Update jars

--- at.jku.dke.aisa.mapperA/src/at/jku/dke/aisa/mapperA/KnowledgeGraphClass.java
+++ at.jku.dke.aisa.mapperA/src/at/jku/dke/aisa/mapperA/KnowledgeGraphClass.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.jena.graph.Graph;
